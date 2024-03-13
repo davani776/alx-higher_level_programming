ad2eab07a557f107ec6443a4258f813b64189a58
Dawit Kassahun <kassahundawit04@gmail.com>
2024-03-12 19:34:12 -0700

    -

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        n = number % -(10)
+        print(-(n), end='')
+    else:
+        n = number % 10
+        print(n, end='')
+    return abs(n)
