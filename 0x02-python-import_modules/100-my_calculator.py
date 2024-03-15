0334eeb63785893ed892a9fdf0e2498153c19c60
davani776 <141369607+davani776@users.noreply.github.com>
2024-03-15 05:44:18 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 import calculator_1
 import sys
-
 def main():
     if len(sys.argv) != 4:
         print("usage: ./100-my_calculator.py <a> <operator> <b>")
         return 1
 
     a = int(sys.argv[1])
-    operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator not in ['+', '-', '*', '/']:
+    if argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
         return 1
 
-    if operator == '+':
+    if argv[2]  == '+':
         print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    elif operator == '-':
+    elif argv[2] == '-':
         print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    elif operator == '*':
+    elif argv[2] == '*':
         print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    elif operator == '/':
+    elif argv[2] == '/':
         if b == 0:
             print("Error: Division by zero!")
             return 1
         print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
-
-    return 0
-
+        return 0
 if __name__ == "__main__":
     sys.exit(main())
 
