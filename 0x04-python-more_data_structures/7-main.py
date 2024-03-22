af1ee38ac42a4beae142915bdbfd52b002f3270c
Dawit Kassahun <kassahundawit04@gmail.com>
2024-03-21 23:18:33 -0700

    -

---
+++ 0x04-python-more_data_structures/7-main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+update_dictionary = __import__('7-update_dictionary').update_dictionary
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+new_dict = update_dictionary(a_dictionary, 'language', "Python")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+print("--")
+print("--")
+
+new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+print_sorted_dictionary(new_dict)
+print("--")
+print_sorted_dictionary(a_dictionary)
+
+guillaume@ubuntu:~/0x04$ ./7-main.py
+language: Python
+number: 89
+track: Low level
+--
+language: Python
+number: 89
+track: Low level
+--
+--
+city: San Francisco
+language: Python
+number: 89
+track: Low level
+--
+city: San Francisco
+language: Python
+number: 89
+track: Low level
+guillaume@ubuntu:~/0x04$ 
+Repo:
+
+GitHub repository: alx-higher_level_programming
+Directory: 0x04-python-more_data_structures
+File: 7-update_dictionary.py
+   
+8. Simple delete by key
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+Write a function that deletes a key in a dictionary.
+
+Prototype: def simple_delete(a_dictionary, key=""):
+key argument will be always a string
+If a key doesn’t exist, the dictionary won’t change
+You are not allowed to import any module
+guillaume@ubuntu:~/0x04$ cat 8-main.py
+#!/usr/bin/python3
+simple_delete = __import__('8-simple_delete').simple_delete
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
+
+print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new = a_dictionary.copy()
+    list_key = list(new_dir.keys())
+    for i in list_key:
+        new[i] = new[i] *  2
+    return (new_dir)
