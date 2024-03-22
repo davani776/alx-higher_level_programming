a6f7a85d3d6e81c0b6134b710e75357813724967
Dawit Kassahun <kassahundawit04@gmail.com>
2024-03-21 23:33:28 -0700

    -

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(my_dict):
+    if my_dict:
+        return max(my_dict, key=my_dict.get)
+    else:
+        None
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new = a_dictionary.copy()
-    list_key = list(new_dir.keys())
+    list_key = list(new())
     for i in list_key:
         new[i] = new[i] *  2
-    return (new_dir)
+    return (new)
